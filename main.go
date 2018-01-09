ff0660182ea4a03184930bec058fa9c591203d03
yemuweishang <18510012428@163.com>
2018-01-09 15:22:54 +0800

    web页面设置加载成功

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-	_ "golangItem/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	_ "golangItem/routers"
+	"net/http"
+	"strings"
 )
 
-func main() {
-	beego.Run()
+func ignoreStaticPath() {
+	//透明static
+	beego.InsertFilter("/", beego.BeforeRouter, TransparentStatic)
+	beego.InsertFilter("/*", beego.BeforeRouter, TransparentStatic)
 }
 
+func TransparentStatic(ctx *context.Context) {
+	orpath := ctx.Request.URL.Path
+	beego.Debug("request url:", orpath)
+	if strings.Index(orpath, "api") >= 0 {
+		return
+	}
+	http.ServeFile(ctx.ResponseWriter, ctx.Request, "static/html/"+ctx.Request.URL.Path)
+}
+
+func main() {
+	ignoreStaticPath()
+	beego.Run()
+}
